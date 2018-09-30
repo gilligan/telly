7c1ed83e5408a5392b2bdf0e59cbf936bb8bb73f
Tobias Pflug <tobias.pflug@holidaycheck.com>
2018-09-30 16:45:12 +0200

    data type and some tests
    - implement simple datatypes according to json output
    - provide ToJSON/FromJSON instances
    - write some basic unit tests

--- src/Telly.hs
+++ src/Telly.hs
@@ -1,4 +1,57 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Telly where
 
-telly :: String
-telly = "telly"
+import Data.Aeson
+import GHC.Generics
+import Data.Text as T
+
+days :: [(Text, Weekday)]
+days = [ ("Monday", Monday)
+       , ("Tuesday", Tuesday)
+       , ("Wednesday", Wednesday)
+       , ("Thursday", Thursday)
+       , ("Friday", Friday)
+       , ("Saturday", Saturday)
+       , ("Sunday", Sunday) ]
+
+data Weekday = Monday 
+             | Tuesday 
+             | Wednesday 
+             | Thursday 
+             | Friday 
+             | Saturday 
+             | Sunday
+             deriving (Show, Eq, Generic)
+
+instance ToJSON Weekday where
+    toJSON x = String $ T.pack . show $ x
+
+
+instance FromJSON Weekday where
+    parseJSON (String s) = case (lookup s days) of
+        (Just d) -> return d
+        Nothing  -> fail "Could not parse"
+    parseJSON _ = fail "Could not parse"
+
+data TvSlot = TVSlot { name :: T.Text
+                     , timeslot :: T.Text
+                     } deriving (Show, Eq, Generic)
+
+instance ToJSON TvSlot
+instance FromJSON TvSlot
+
+data ChannelProg = ChannelProg { channel :: T.Text
+                               , programming :: [TvSlot]
+                               } deriving (Show, Eq, Generic)
+
+instance ToJSON ChannelProg
+instance FromJSON ChannelProg
+
+data DayProg = DayProg { channels :: [ChannelProg]
+                       , day :: Weekday
+                       } deriving (Show, Eq, Generic)
+
+instance ToJSON DayProg
+instance FromJSON DayProg
--- test/TellySpec.hs
+++ test/TellySpec.hs
@@ -1,10 +1,61 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module TellySpec where
 
+import qualified Data.ByteString.Lazy as BS
+import qualified Data.Aeson as AE
+import Data.Maybe 
+
 import Test.Tasty.Hspec
+
 import Telly
 
-spec :: Spec
-spec = do
+spec_Telly :: Spec
+spec_Telly = do
     describe "Telly" $ do
-        it "exports a useless string" $
-            Telly.telly `shouldBe` "telly"
+
+        describe "Weekday" $ do
+            it "should deserialize as expected" $ do
+                AE.decode "\"Monday\"" `shouldBe` (Just Monday)
+                AE.decode "\"Tuesday\"" `shouldBe` (Just Tuesday)
+                AE.decode "\"Wednesday\"" `shouldBe` (Just Wednesday)
+                AE.decode "\"Thursday\"" `shouldBe` (Just Thursday)
+                AE.decode "\"Friday\"" `shouldBe` (Just Friday)
+                AE.decode "\"Saturday\"" `shouldBe` (Just Saturday)
+                AE.decode "\"Sunday\"" `shouldBe` (Just Sunday)
+            it "should serialize as expected" $ do
+                AE.encode Monday `shouldBe` "\"Monday\"" 
+                AE.encode Tuesday `shouldBe` "\"Tuesday\""
+                AE.encode Wednesday `shouldBe` "\"Wednesday\""
+                AE.encode Thursday `shouldBe`"\"Thursday\""
+                AE.encode Friday `shouldBe` "\"Friday\""
+                AE.encode Saturday `shouldBe`"\"Saturday\""
+                AE.encode Sunday `shouldBe` "\"Sunday\""
+
+        describe "TVSlot" $ do
+            it "should deserialize as expected" $
+                AE.decode "{\"name\":\"disneyclub\",\"timeslot\":\"13:00\"}" `shouldBe` (Just $ TVSlot "disneyclub" "13:00")
+            it "should serialize as expected" $
+                AE.encode (Just $ TVSlot "disneyclub" "13:00") `shouldBe` "{\"name\":\"disneyclub\",\"timeslot\":\"13:00\"}"
+
+        describe "ChannelProg" $ do
+            it "should deserialize as expected" $ do
+                AE.decode "{\"channel\":\"Kids\",\"programming\":[{\"name\":\"disneyclub\",\"timeslot\":\"13:00\"}]}" `shouldBe` (Just $ ChannelProg "Kids" [TVSlot "disneyclub" "13:00"])
+            it "should serialize as expected" $ do
+                AE.encode (Just $ ChannelProg "Kids" [TVSlot "disneyclub" "13:00"]) `shouldBe` "{\"channel\":\"Kids\",\"programming\":[{\"name\":\"disneyclub\",\"timeslot\":\"13:00\"}]}"
+
+        describe "DayProg" $ do
+            it "should deserialize as expected" $ do
+                AE.decode "{\"channels\":[{\"channel\":\"Kids\",\"programming\":[{\"name\":\"disneyclub\",\"timeslot\":\"13:00\"}]}],\"day\":\"Monday\"}" `shouldBe` (Just (DayProg [(ChannelProg "Kids" [TVSlot "disneyclub" "13:00"])] Monday))
+            it "should serialize as expected" $ do
+                AE.encode (DayProg [(ChannelProg "Kids" [TVSlot "disneyclub" "13:00"])] Monday) `shouldBe` "{\"channels\":[{\"channel\":\"Kids\",\"programming\":[{\"name\":\"disneyclub\",\"timeslot\":\"13:00\"}]}],\"day\":\"Monday\"}"
+
+        describe "sample.json" $ do
+            it "can be deserialized" $ do
+                sampleData <- BS.readFile "./sample.json"
+                isJust (AE.decode sampleData :: Maybe [DayProg]) `shouldBe` True
+            it "records program for 7 days" $ do
+                sampleData <- BS.readFile "./sample.json"
+                let program = (AE.decode sampleData :: Maybe [DayProg])
+                length <$> program `shouldBe` (Just 7)
